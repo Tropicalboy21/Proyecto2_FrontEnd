3cfbb129eddf0d6881631456d9fedd2f79b0e508
AndresPMCenfotec <apizarrom@ucenfotec.ac.cr>
2024-11-10 14:36:47 -0600

    CambiosHome

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,34 @@
 import React from "react";
-import {Link} from "react-router-dom"
-import '../assets/styles/navbar.css'
-const Navbar = ({username}) =>{
+import { Link } from "react-router-dom";
+import '../assets/styles/navbar.css';
 
-return(<div>
-     <header className="header">
-        <div className="logo"></div>
-        <nav className="nav">
-        <Link to="/home"><button >HOME</button></Link>
-          <Link to="/multas"><button >MULTAS</button></Link>
-          <button>VEHICULOS</button>
-          <button>DISPUTAS</button>
-        </nav>
-        <div className="user-info">Nombre de Usuario</div>
-      </header>
-</div>);
+const Navbar = ({ username }) => {
+  return (
+    <header className="header">
+      <div className="logo"> </div>
+      <nav className="nav">
+        <Link to="/home">
+          <button className="nav-button">INICIO</button>
+        </Link>
+        <Link to="/multas">
+          <button className="nav-button">MULTAS</button>
+        </Link>
+        <Link to="/vehiculos">
+          <button className="nav-button">VEHICULOS</button>
+        </Link>
+        <Link to="/disputas">
+          <button className="nav-button">DISPUTAS</button>
+        </Link>
+        <Link to="/historial">
+          <button className="nav-button">HISTORIAL</button>
+        </Link>
+        <Link to="/mapa">
+          <button className="nav-button">MAPA</button>
+        </Link>
+      </nav>
+      <div className="user-info">{username ? `Usuario: ${username}` : 'Usuario'}</div>
+    </header>
+  );
 }
 
 export default Navbar;
