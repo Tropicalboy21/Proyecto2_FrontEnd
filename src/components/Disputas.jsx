15050b85f8aa46aa3fb68a0949ae954c90cba1b1
Kevin Torres <125518994+ktr60@users.noreply.github.com>
2024-11-10 17:37:37 -0600

    update

--- src/components/Disputas.jsx
+++ src/components/Disputas.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import '../App.css'
 import '../assets/styles/multas.css'
+import {Link} from "react-router-dom"
 const Disputas = ({username}) =>{
 
     return(<div className='view-container'> 
         <main className="main-content">
         <h1 className='title'>Disputas</h1>
+        <table className="fines-table">
+          <thead>
+            <tr>
+              <th>MULTAS</th>
+              <th>RAZON</th>
+              <th>MONTO</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>MULTA1</td>
+              <td>RAZON</td>
+              <td>MONTO</td>
+            </tr>
+            <tr>
+              <td>MULTA2</td>
+              <td>RAZON</td>
+              <td>MONTO</td>
+            </tr>
+            <tr>
+              <td>MULTA3</td>
+              <td>RAZON</td>
+              <td>MONTO</td>
+            </tr>
+          </tbody>
+        </table>
         <div className='Buttons'>
-        <button className="iniciar login">Regresar</button>
+        <Link to="/home"><button className="iniciar login" >Regresar</button></Link> 
         </div>
       </main>
     </div>);
