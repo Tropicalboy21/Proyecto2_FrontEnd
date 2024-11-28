705767ba6d42d2f53e69abb03f970f156e6376b4
Kevin Torres <125518994+ktr60@users.noreply.github.com>
2024-11-27 19:35:09 -0600

    update pagoCiudadano

--- src/components/Ciudadano/PagoCiudadano.jsx
+++ src/components/Ciudadano/PagoCiudadano.jsx
@@ -3,28 +3,31 @@ import { useNavigate, Link } from 'react-router-dom';
 import '../../assets/styles/app.css';
 import ImagenLogo from '../../assets/imgs/logo.png';
 import Alert from '../Alert';
-import multas from '../../assets/data/multas.json'; // Import JSON file
 
-const PagoMulta = ({ username }) => {
-  const [amount, setAmount] = useState('');
-  const [description, setDescription] = useState('');
-  const [useremail, setUserEmail] = useState('');
+const PagoMulta = () => {
+  const [cardNumber, setCardNumber] = useState('');
+  const [cardDate, setCardDate] = useState('');
+  const [cardName, setCardName] = useState('');
+  const [cardCode, setCardCode] = useState('');
+
   const [showAlert, setShowAlert] = useState(false);
   const [alertType, setAlertType] = useState('');
   const [alertMessage, setAlertMessage] = useState('');
 
-  const registerFine = async (e) => {
+  const fineId = localStorage.getItem('username');
+
+  const registerPayment = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('https://localhost:7289/api/Fines', {
+      const response = await fetch('https://localhost:7289/api/Payment', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          amount,
-          description,
-          userEmail: useremail,
+          fineId: "2525",
+          amount: "1000",
+          paymentDate: "2024-11-28", 
         }),
       });
 
@@ -51,45 +54,45 @@ const PagoMulta = ({ username }) => {
   return (
     <div className="view-container">
       {showAlert && <Alert type={alertType} message={alertMessage} onClose={handleCloseAlert} />}
-      <form onSubmit={registerFine} className="padre2">
+      <form onSubmit={registerPayment} className="padre2">
         <img src={ImagenLogo} alt="imagelogo" className="tamano-imagen" />
         <h2 className="title">Pago de Multa</h2>
         <div className="Inputs">
 
-        <p>Id de Multa: </p>   
-        <p>Fecha de Multa:</p>
+        <p>Id de Multa: {fineId} </p>   
+        <p>Fecha de Multa: {fineId}</p>
 
         <p>Numero de la Tarjeta: </p>   
           <input
             type="text"
             placeholder="XXXX-XXXX-XXXX-XXXX"
-            onChange={(e) => setUserEmail(e.target.value)}
+            onChange={(e) => setCardNumber(e.target.value)}
             required
           />
           <p>Fecha de Caducidad: </p>   
             <input
             type="text"
             placeholder="MM/AA"
-            onChange={(e) => setUserEmail(e.target.value)}
+            onChange={(e) => setCardDate(e.target.value)}
             required
           />
             <p>Titular de la Tarjeta: </p> 
             <input
             type="text"
             placeholder="Nombre Apellido"
-            onChange={(e) => setUserEmail(e.target.value)}
+            onChange={(e) => setCardName(e.target.value)}
             required
           />
             <p>CVV: </p> 
           <input
             type="text"
             placeholder="XXX"
-            onChange={(e) => setDescription(e.target.value)}
+            onChange={(e) => setCardCode(e.target.value)}
             required
           />
     
 
-          <p>Monto a Pagar: ₡ {amount}</p>
+          <p>Monto a Pagar: ₡ {fineId}</p>
 
         </div>
         <div className="Buttons">
