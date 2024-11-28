a06cddb302de65261eb9edbc372534c1c933caa1
Kevin Torres <125518994+ktr60@users.noreply.github.com>
2024-11-27 18:03:13 -0600

    Update PagoCiudadano.jsx

--- src/components/Ciudadano/PagoCiudadano.jsx
+++ src/components/Ciudadano/PagoCiudadano.jsx
@@ -1,91 +1,105 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import '../../assets/styles/app.css';
-import '../../assets/styles/stylesCiudadanos/multasCiudadano.css';
+import ImagenLogo from '../../assets/imgs/logo.png';
+import Alert from '../Alert';
+import multas from '../../assets/data/multas.json'; // Import JSON file
 
-const Multas = () => {
-  const [fines, setFines] = useState([]);
-  const [error, setError] = useState(null);
+const PagoMulta = ({ username }) => {
+  const [amount, setAmount] = useState('');
+  const [description, setDescription] = useState('');
+  const [useremail, setUserEmail] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertType, setAlertType] = useState('');
+  const [alertMessage, setAlertMessage] = useState('');
 
-  const username = localStorage.getItem('username');
-  
-  useEffect(() => {
-    const fetchFines = async () => {
-      try {
-        
-        const response = await fetch(`https://localhost:7289/api/Fines?userin=${username}`, {
-          
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        });
+  const registerFine = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('https://localhost:7289/api/Fines', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          amount,
+          description,
+          userEmail: useremail,
+        }),
+      });
 
-        if (!response.ok) {
-          throw new Error(`Error: ${response.statusText}`);
-        }
-
-        const data = await response.json();
-        
-
-        
-        if (Array.isArray(data) && data.length > 0) {
-          setFines(data);
-        } else {
-          setFines([]); 
-        }
-      } catch (err) {
-        setError(err.message);
+      if (!response.ok) {
+        setAlertMessage('Error al pagar la multa');
+        setAlertType('error');
+      } else {
+        setAlertMessage('¡Multa pagada con éxito!');
+        setAlertType('success');
       }
-    };
-
-    
-    if (username) {
-      fetchFines();
+      setShowAlert(true);
+    } catch (error) {
+      setAlertMessage(error.message === 'Failed to fetch' ? 'Lo sentimos, error en la conexión' : error.message);
+      setAlertType('error');
+      setShowAlert(true);
     }
-  }, [username]);
+  };
 
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+    setAlertMessage('');
+  };
 
   return (
     <div className="view-container">
-      <main className="main-content">
-        <h1 className="title">Pago de Multa</h1>
+      {showAlert && <Alert type={alertType} message={alertMessage} onClose={handleCloseAlert} />}
+      <form onSubmit={registerFine} className="padre2">
+        <img src={ImagenLogo} alt="imagelogo" className="tamano-imagen" />
+        <h2 className="title">Pago de Multa</h2>
+        <div className="Inputs">
 
-        
-        {error ? (
-          <p className="error-message">Error al cargar las multas: {error}</p>
-        ) : (
-          <table className="fines-table">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Razón</th>
-                <th>Monto</th>
-                <th>Fecha</th>
-              </tr>
-            </thead>
-            <tbody>
-              
-              {fines.length > 0 ? (
-                fines.map((fine) => (
-                  <tr key={fine.id}>
-                    <td>{fine.id}</td>
-                    <td>{fine.description}</td>
-                    <td>₡{fine.amount}</td>
-                    <td>{new Date(fine.issuedDate).toLocaleDateString()}</td>
-                  </tr>
-                ))
-              ) : (
-                <tr>
-                  <td colSpan="4">No se encontraron multas.</td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        )}
+        <p>Id de Multa: </p>   
+        <p>Fecha de Multa:</p>
 
-      </main>
+        <p>Numero de la Tarjeta: </p>   
+          <input
+            type="text"
+            placeholder="XXXX-XXXX-XXXX-XXXX"
+            onChange={(e) => setUserEmail(e.target.value)}
+            required
+          />
+          <p>Fecha de Caducidad: </p>   
+            <input
+            type="text"
+            placeholder="MM/AA"
+            onChange={(e) => setUserEmail(e.target.value)}
+            required
+          />
+            <p>Titular de la Tarjeta: </p> 
+            <input
+            type="text"
+            placeholder="Nombre Apellido"
+            onChange={(e) => setUserEmail(e.target.value)}
+            required
+          />
+            <p>CVV: </p> 
+          <input
+            type="text"
+            placeholder="XXX"
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+    
+
+          <p>Monto a Pagar: ₡ {amount}</p>
+
+        </div>
+        <div className="Buttons">
+          <button className="iniciar login" type="submit">
+            Pagar
+          </button>
+        </div>
+      </form>
     </div>
   );
+};
 
-}
-export default Multas;
+export default PagoMulta;
